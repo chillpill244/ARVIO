a416abafc04866ee7e50de3a8ef91615713f1acf
chill pill 244 <chillpill244@gmail.com>
2026-06-03 20:04:44 -0700

    chore: bump version to 2.1.3 (versionCode 285)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         // Fire TV devices can be as low as Android 7.1 (API 25) or lower depending on model/OS.
         minSdk = 23
         targetSdk = 35
-        versionCode = 284
-        versionName = "2.1.2"
+        versionCode = 285
+        versionName = "2.1.3"
         buildConfigField("String", "GITHUB_OWNER", "\"chillpill244\"")
         buildConfigField("String", "GITHUB_REPO", "\"ARVIO\"")
 
